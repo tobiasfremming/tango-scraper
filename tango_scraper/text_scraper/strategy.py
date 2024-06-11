518d4fe4af0b2c3f6d9e69e02e2e5bb468efb08d
Tobias Fremming <tobif@stud.ntnu.no>
2024-06-11 18:54:14 +0200

    refactor: make classes not callable, and add isReadable te see if a pdf is readable or not, so it can be used in the factory.

--- tango_scraper/text_scraper/strategy.py
+++ tango_scraper/text_scraper/strategy.py
@@ -10,18 +10,19 @@ from text_scraper.doc_reader import DocReader
 from text_scraper.text_reader import TextReader
 from text_scraper.post_processing import PostProcessor
 from text_scraper.ocr import OCR
+from text_scraper.soundScraper import SoundScraper
 from abc import ABC, abstractmethod
 
 class Strategy(ABC):
     
     @abstractmethod
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
         pass
     
     
 class ReadPDFStrategy(Strategy):
     
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
         # TODO: implement the correct logic
         print("Extracting text from PDF file")
         reader: TextReader = TextReader()
@@ -30,36 +31,37 @@ class ReadPDFStrategy(Strategy):
         
         pages.extend(reader.read(file))
         data = post_processor.page_post_processing(pages)
+        
         return data
     
     
 class OCRStrategy(Strategy):
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
         # TODO: implement the correct logic
         print("Extracting text from image file")
         
         return []
     
 class ReadDocStrategy(Strategy):
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
         # TODO: implement the correct logic
         print("Extracting text from Word document")
         return []
         
-class Mp3Strategy(Strategy):
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
-        # TODO: implement the correct logic
-        print("Extracting text from MP3 file")
-        return []
+class SoundStrategy(Strategy):
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
+        
+        print("Extracting text from Sound file")
+        return SoundScraper.speech_to_text(file)
         
 class EpubStrategy(Strategy):
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
         # TODO: implement the correct logic
         print("Extracting text from EPUB file")
         return []
         
 class URLStrategy(Strategy):
-    def __call__(self, file: InMemoryUploadedFile) -> list[Page]:
+    def execute(self, file: InMemoryUploadedFile) -> list[Page]:
         # TODO: implement the correct logic
         print("Extracting text from URL")
         return []
@@ -75,12 +77,45 @@ class StrategyFactory:
                 return OCRStrategy()
             case "doc" | "docx":
                 return ReadDocStrategy()
-            case "mp3":
-                return Mp3Strategy()
+            case "mp3"| "wav" | "flac" | "ogg" | "m4a":
+                return SoundStrategy()
             case "epub":
                 return EpubStrategy()
             case "url":
                 return URLStrategy()
             case _:
                 return OCRStrategy()
+            
+            
+    def _isReadable(self, file: InMemoryUploadedFile) -> bool:
+        """
+        Checks if a PDF file is easily readable by attempting to extract text directly from it.
+
+        This method does not guarantee OCR accuracy but checks if the PDF contains selectable text,
+        which is a good indicator of the document's readability without needing image conversion.
+
+        Args:
+            file: The PDF file to check.
+
+        Returns:
+            True if the file is easily readable (contains a significant amount of selectable text),
+            False otherwise.
+        """
+
+        total_pages = self.reader.get_amount_of_pages(file)
+        fast_text = ""
+        ocr_text = ""
+        ocr = OCR(file)
+
+        # Look random pages and see if there is a large disparity
+        for _ in range(3):  # TODO: change to 3 with log2(total_pages1)
+            page_number = random.randint(0, total_pages - 1)
+            fast_text += self.reader.read_page(file, page_number)
+            ocr_text += ocr.ocr_page(file, page_number)
+
+        if len(ocr_text) == 0:
+            return True  # TODO: lag feilmelding
+        if len(fast_text) / len(ocr_text) > 0.88:
+            return True
+        return False
         
